18c6fc55b406115b7bbfb7949e61019165ffd00b
Dharmendra Patel <dharmpatel09@gmail.com>
2024-06-08 10:13:14 +0530

    changes in home controller

--- HelloDevopsGit/Controllers/HomeController.cs
+++ HelloDevopsGit/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace HelloDevopsGit.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+
         }
 
         public IActionResult Index()
