73e8345118b415e7de842add48c92d588204edea
Dharmendra Patel <dharmpatel09@gmail.com>
2025-03-25 11:44:48 +0530

    Initial commit gh-pages

--- HelloDevopsGit/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HelloDevopsGit.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
